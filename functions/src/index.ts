94dbf2cf124437d85d2ff2ae68946d5a08a55d59
ryokada <ryokada.5088[gmail.com>
2023-10-17 22:16:53 +0900

    環境変数周りの設定

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,5 @@ export const helloWorld = functions
     .region('asia-northeast1')
     .https.onRequest((request, response) => {
         logger.info('Hello logs!', { structuredData: true });
-        response.send('Hello from Firebase!!!!!!');
+        response.send(`Hello ${process.env.TEST} from Firebase!!!!!!`);
     });
